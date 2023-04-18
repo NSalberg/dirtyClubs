200e8b73ced26e97ed2dbf9fddfb5c9e21e1b1ff
Nathan Salberg <Salbe018@d.umn.edu>
2023-04-18 15:59:05 -0500

    Added find playable function to fill hand_playable with playable cards

--- players.py
+++ players.py
@@ -5,7 +5,7 @@ class Player:
     def __init__(self, name: str) -> None:
         self.name = name
         self.hand = list[Card]()
-        self.hand_play = list[Card]()
+        self.hand_playable = list[Card]()
         self.score: int = 0
 
 
@@ -14,17 +14,20 @@ class Player:
         self.action = 0
         
     def find_playable(self, lead_card: Optional[Card]) -> None:
-        self.hand_play = list[Card]()
-        if lead_card is None:
+        """
+        Looks at hand and finds playable cards, then adds them to hand_play
+        lead_card: card that was played first if first card in trick lead_card is None
+        """
+        self.hand_playable = list[Card]()
+        #if lead_card is None or player does not have suit of lead_card add all cards to playable
+        if lead_card is None or not self.has_suit(lead_card.suit):
             for card in self.hand:
-                self.hand_play.append(card)
-        elif self.has_suit(lead_card.suit):
-            for card in self.hand:
-                if  card.suit == lead_card.suit:
-                    self.hand_play.append(card)
+                self.hand_playable.append(card)
         else:
             for card in self.hand:
-                self.hand_play.append(card)
+                if  card.suit == lead_card.suit:
+                    self.hand_playable.append(card)
+
     
     def has_suit(self, suit: str) -> bool:
         for card in self.hand:
