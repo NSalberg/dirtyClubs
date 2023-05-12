a56ad949d1bf95292aa4e4c21b3578795f1a2c61
Nathan Salberg <Salbe018@d.umn.edu>
2023-05-12 15:57:34 -0500

    Added player_mdp class

--- players.py
+++ players.py
@@ -6,7 +6,7 @@ from abc import abstractmethod, ABC
 
 class Player(ABC):
     @abstractmethod
-    def __init__(self, name: str, hand) -> None:
+    def __init__(self, name: str, hand: list[Card]) -> None:
         self.name = name
         self.hand = hand
         self.hand_playable = list[Card]()
@@ -127,3 +127,52 @@ class Player_Random(Player):
             self.in_play.clear()
             self.lead_card = None
         
+class Player_MDP(Player):
+    def __init__(self, name: str, hand: list[Card], deck: Deck) -> None:
+        self.name = name
+        self.hand = hand
+        self.hand_playable = list[Card]()
+        self.deck = deck
+        self.score: int = 0
+        self.in_play = list[Card]()
+        self.seen = list[Card]()
+        self.card_map = defaultdict(list)
+        for card in hand:
+            self.card_map[card.suit].append(card)    
+    
+    def play_card(self, lead_card: Optional[Card]) -> Card:
+        """
+        play_card plays a card from the hand_playable
+        
+        lead_card: card that was played first if it is the first card in the trick lead_card is None
+        """
+        self.find_playable(lead_card)
+        played_card = random.choice(self.hand_playable)
+        self.hand.remove(played_card)
+
+
+        player_cards = []
+        for card in self.deck.cards:
+            if card in self.in_play:
+                player_cards.append("in_play")
+            elif card in self.seen:
+                player_cards.append("seen")
+            elif card in self.hand:
+                player_cards.append("m1")
+                continue
+            else:
+                player_cards.append("m2")
+        print(self.name + str(player_cards))
+    
+        return played_card
+    
+    def observeActionTaken(self, player, card: Card) -> None:
+        if player == self:
+            self.card_map[card.suit].remove(card)
+        self.in_play.append(card) 
+
+        #TODO: this should only happen when the trick is over change 4 to however many players there are
+        if len(self.in_play) == 4:
+            self.seen.extend(self.in_play)
+            self.in_play.clear()
+            self.lead_card = None
